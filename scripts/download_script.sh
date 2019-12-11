3f88f9baf88542e8da5a88a9acad48c87eac3c99
ormaniec <wojciech.ormaniec@gmail.com>
2019-12-11 22:31:27 +0100

    [**UPD**] Fix minor mistakes

--- scripts/download_script.sh
+++ scripts/download_script.sh
@@ -4,7 +4,7 @@ function download()
 	wget https://ddragon.leagueoflegends.com/cdn/9.24.1/img/champion/${1}.png --directory-prefix=../src/main/resources/images/champions
 }
 
-input="list"
+input="../src/main/resources/data/list.txt"
 while IFS= read -r line
 do
         echo "Downloading $line"  
