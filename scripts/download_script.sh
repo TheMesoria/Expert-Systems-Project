2e3b3e783e5125708b31d07afdb7b31c90fb0d2b
ormaniec <wojciech.ormaniec@gmail.com>
2019-12-11 22:26:49 +0100

    [**NEW**] Add script to download images

---
+++ scripts/download_script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+function download()
+{
+	wget https://ddragon.leagueoflegends.com/cdn/9.24.1/img/champion/${1}.png --directory-prefix=../src/main/resources/images/champions
+}
+
+input="list"
+while IFS= read -r line
+do
+        echo "Downloading $line"  
+        download ${line}
+done < "$input"
+
