9774b0501fca823a378b991a46414e7bbf5c69f2
ormaniec <wojciech.ormaniec@gmail.com>
2019-12-12 20:22:21 +0100

    [**NEW**] Add controller with fix for wrong id in fxml

--- src/main/java/controller/handler/MainViewController.java
+++ src/main/java/controller/handler/MainViewController.java
@@ -1,5 +1,37 @@
 package controller.handler;
 
+import com.jfoenix.controls.JFXButton;
+import javafx.fxml.FXML;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
 public class MainViewController
 {
+
+    @FXML private HBox m_RedTeamHBox;
+    @FXML private VBox m_RedTeamBanVBox;
+    @FXML private JFXButton m_RedBanButton_1;
+    @FXML private JFXButton m_RedBanButton_2;
+    @FXML private JFXButton m_RedBanButton_3;
+    @FXML private JFXButton m_RedBanButton_4;
+    @FXML private JFXButton m_RedBanButton_5;
+    @FXML private JFXButton m_RedTeamPickButton_1;
+    @FXML private JFXButton m_RedTeamPickButton_11;
+    @FXML private JFXButton m_RedTeamPickButton_12;
+    @FXML private JFXButton m_RedTeamPickButton_13;
+    @FXML private JFXButton m_RedTeamPickButton_14;
+
+    @FXML private HBox m_BlueTeamHBox;
+    @FXML private VBox m_BlueTeamBanVBox;
+    @FXML private JFXButton m_BlueBanButton_1;
+    @FXML private JFXButton m_BlueBanButton_2;
+    @FXML private JFXButton m_BlueBanButton_3;
+    @FXML private JFXButton m_BlueBanButton_4;
+    @FXML private JFXButton m_BlueBanButton_5;
+    @FXML private JFXButton m_BlueTeamPickButton_1;
+    @FXML private JFXButton m_BlueTeamPickButton_2;
+    @FXML private JFXButton m_BlueTeamPickButton_3;
+    @FXML private JFXButton m_BlueTeamPickButton_4;
+    @FXML private JFXButton m_BlueTeamPickButton_5;
+
 }
