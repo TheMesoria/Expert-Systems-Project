3c692ae8a353524fbb261190c7b1f84ce538b8c4
ormaniec <wojciech.ormaniec@gmail.com>
2020-01-01 01:07:08 +0100

    [**TMP**] Backup

--- src/main/java/controller/view/util/ChampionPicker.java
+++ src/main/java/controller/view/util/ChampionPicker.java
@@ -1,18 +1,17 @@
 package controller.view.util;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jfoenix.controls.JFXMasonryPane;
 import com.jfoenix.controls.JFXTextField;
-import controller.manager.ViewStorage;
-import controller.view.MainViewController;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 
+import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 public class ChampionPicker
@@ -22,34 +21,27 @@ public class ChampionPicker
     @FXML private JFXTextField m_ChampionNameTextBox;
     @FXML private JFXMasonryPane m_ChampionPool;
 
+    private ArrayList<Object> m_Champions;
+
     @Override public void initialize(URL url,
                                      ResourceBundle resourceBundle)
     {
-        var image = new Image("file:/home/black/Work/code/college/Expert-Systems-Project/src/main/resources/images" +
-                "/champions/Aatrox.png");
+        try
+        {
+            m_Champions =
+                    new ObjectMapper().readValue(getClass().getResource("/data/list.json"), new TypeReference<>() {});
 
-        var imageView = new ImageView(
-                image
-        );
-
-        imageView.setScaleX(0.5);
-        imageView.setScaleY(0.5);
-
-        m_ChampionPool.getChildren().add(
-                imageView
-                );
+            System.out.println(m_Champions.get(0).getClass().getSimpleName());
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
 
         m_ChampionNameTextBox.focusedProperty().addListener(
-                new ChangeListener<Boolean>() {
-                    @Override public void changed(ObservableValue<? extends Boolean> observableValue,
-                                                  Boolean oldValue,
-                                                  Boolean newValue)
-                    {
-                        if(newValue)
-                        {
-                            m_ChampionNameTextBox.clear();
-                        }
-                    }
+                (observableValue, oldValue, newValue) ->
+                {
+                    if (newValue) { m_ChampionNameTextBox.clear(); }
                 }
                                                            );
     }
